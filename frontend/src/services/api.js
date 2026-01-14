e8f5303c5136e6aaa66dad30c45805f7495f52c6
yamusang <ttobiyam@naver.com>
2026-01-14 16:51:29 +0900

    fix url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8787';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://20260114cicd.ttobiyam.workers.dev:8787';
 
 export const api = {
     // 모든 퀴즈 가져오기
